61c40d1f6a48c957492bce8be84474bf4487f780
unknown <joshlin8889@gmail.com>
2024-05-13 19:31:00 +0800

    change ID to uuid

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,97 +1,113 @@
-import { relations } from "drizzle-orm";
+import { sql, relations } from "drizzle-orm";
 import {
   index,
   text,
   pgTable,
-  serial,
   uuid,
   varchar,
   unique,
+  boolean,
+  timestamp,
 } from "drizzle-orm/pg-core";
 
-// Checkout the many-to-many relationship in the following tutorial:
-// https://orm.drizzle.team/docs/rqb#many-to-many
-
-export const usersTable = pgTable(
-  "users",
+// 學生使用者資料表
+export const studentUserTable = pgTable(
+  "student_users",
   {
-    id: serial("id").primaryKey(),
-    displayId: uuid("display_id").defaultRandom().notNull().unique(),
-    username: varchar("username", { length: 100 }).notNull(),
+    id: uuid("id").primaryKey(),  // 使用uuid生成學生ID
+    name: varchar("name", { length: 100 }).notNull(),
     email: varchar("email", { length: 100 }).notNull().unique(),
-    hashedPassword: varchar("hashed_password", { length: 100 }),
-    provider: varchar("provider", {
-      length: 100,
-      enum: ["github", "credentials"],
-    })
-      .notNull()
-      .default("credentials"),
+    class: varchar("class_name", { length: 100 }).notNull(),
+    parentEmail: varchar("parent_email", { length: 100 }).notNull(),
   },
   (table) => ({
-    displayIdIndex: index("display_id_index").on(table.displayId),
     emailIndex: index("email_index").on(table.email),
   }),
 );
 
-export const usersRelations = relations(usersTable, ({ many }) => ({
-  usersToDocumentsTable: many(usersToDocumentsTable),
-}));
-
-export const documentsTable = pgTable(
-  "documents",
+// 老師使用者資料表
+export const teacherUserTable = pgTable(
+  "teacher_users",
   {
-    id: serial("id").primaryKey(),
-    displayId: uuid("display_id").defaultRandom().notNull().unique(),
-    title: varchar("title", { length: 100 }).notNull(),
-    content: text("content").notNull(),
+    id: uuid("id").primaryKey(),  // 使用uuid生成教師ID
+    name: varchar("name", { length: 100 }).notNull(),
+    email: varchar("email", { length: 100 }).notNull().unique(),
   },
   (table) => ({
-    displayIdIndex: index("display_id_index").on(table.displayId),
+    emailIndex: index("email_index").on(table.email),
   }),
 );
 
-export const documentsRelations = relations(documentsTable, ({ many }) => ({
-  usersToDocumentsTable: many(usersToDocumentsTable),
-}));
-
-export const usersToDocumentsTable = pgTable(
-  "users_to_documents",
+// 班級資料表
+export const classTable = pgTable(
+  "classes",
   {
-    id: serial("id").primaryKey(),
-    userId: uuid("user_id")
+    id: uuid("id").primaryKey(),
+    name: varchar("name", { length: 100 }).notNull(),
+    teacherId: uuid("teacher_id")
       .notNull()
-      .references(() => usersTable.displayId, {
-        onDelete: "cascade",
-        onUpdate: "cascade",
-      }),
-    documentId: uuid("document_id")
-      .notNull()
-      .references(() => documentsTable.displayId, {
+      .references(() => teacherUserTable.id, {
         onDelete: "cascade",
         onUpdate: "cascade",
       }),
   },
   (table) => ({
-    userAndDocumentIndex: index("user_and_document_index").on(
-      table.userId,
-      table.documentId,
-    ),
-    // This is a unique constraint on the combination of userId and documentId.
-    // This ensures that there is no duplicate entry in the table.
-    uniqCombination: unique().on(table.documentId, table.userId),
+    teacherIndex: index("teacher_index").on(table.teacherId),
   }),
 );
 
-export const usersToDocumentsRelations = relations(
-  usersToDocumentsTable,
-  ({ one }) => ({
-    document: one(documentsTable, {
-      fields: [usersToDocumentsTable.documentId],
-      references: [documentsTable.displayId],
-    }),
-    user: one(usersTable, {
-      fields: [usersToDocumentsTable.userId],
-      references: [usersTable.displayId],
-    }),
+// 圖片資料表
+export const pictureTable = pgTable(
+  "pictures",
+  {
+    id: uuid("id").primaryKey(),
+    image: varchar("image").notNull(),
+    text: text("text").notNull(),
+    date: timestamp("date")
+      .notNull()
+      .default(sql`now()`),
+  },
+  (table) => ({}),
+);
+
+// 繪本資料表
+export const pictureBookTable = pgTable(
+  "picture_books",
+  {
+    id: uuid("id").primaryKey(),
+    pictureId: uuid("picture_id")
+      .notNull()
+      .references(() => pictureTable.id, {
+        onDelete: "cascade",
+        onUpdate: "cascade",
+      }),
+    sendEmail: boolean("send_email").notNull().default(false),
+  },
+  (table) => ({
+    pictureIndex: index("picture_index").on(table.pictureId),
   }),
 );
+
+// 學生使用者關聯
+export const studentUserRelations = relations(studentUserTable, ({ one }) => ({
+  pictureBooks: one(pictureBookTable, {
+    fields: [studentUserTable.id],
+    references: [pictureBookTable.pictureId],
+  }),
+}));
+
+// 老師使用者關聯
+export const teacherUserRelations = relations(teacherUserTable, ({ one }) => ({
+  classes: one(classTable, {
+    fields: [teacherUserTable.id],
+    references: [classTable.teacherId],
+  }),
+}));
+
+// 繪本關聯
+export const pictureBookRelations = relations(pictureTable, ({ one }) => ({
+  book: one(pictureBookTable, {
+    fields: [pictureTable.id],
+    references: [pictureBookTable.pictureId],
+  }),
+}));
