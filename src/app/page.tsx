66ea6d318187f6c15c519cdd12df00743af4b25d
polinchen <90204339+berlin0308@users.noreply.github.com>
2024-05-15 15:53:20 +0800

    login v1 font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,15 @@
+import * as React from "react";
+
 import localFont from "next/font/local";
+
 // import Image from "next/image";
-import Link from "next/link";
+// import Link from "next/link";
 
 const pattaya = localFont({
   src: "./fonts/Poetsen-One.woff2",
   weight: "700",
   style: "italic",
 });
-
-// export default function Home() {
-//   return (
-//     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-//       Hello world ! This is the home page
-//       <Link
-//         href="/painting"
-//         className={`${pattaya.className} text-txt mb-4 text-center text-5xl`}
-//       >
-//         Start Painting
-//       </Link>
-//     </main>
-//   );
-// }
-
-import * as React from "react";
 
 interface HeroSectionProps {
   title: string;
@@ -40,27 +27,29 @@ const HeroSection: React.FC<HeroSectionProps> = ({
   imageAlt,
 }) => {
   return (
-    <section className="flex justify-center items-center px-16 py-20 bg-amber-100 max-md:px-5">
+    <section className="flex items-center justify-center bg-amber-100 px-16 py-20 max-md:px-5">
       <div className="mt-20 w-full max-w-[1332px] max-md:mt-10 max-md:max-w-full">
         <div className="flex gap-5 max-md:flex-col max-md:gap-0">
-          <div className="flex flex-col w-6/12 max-md:ml-0 max-md:w-full">
-            <div className="flex flex-col self-stretch my-auto text-center max-md:mt-10 max-md:max-w-full">
-              <h1 className="text-5xl text-orange-400 max-md:max-w-full max-md:text-4xl">
+          <div className="flex w-6/12 flex-col max-md:ml-0 max-md:w-full">
+            <div className="my-auto flex flex-col self-stretch text-center max-md:mt-10 max-md:max-w-full">
+              <h1
+                className={`${pattaya.className} text-txt mb-4 text-center text-5xl`}
+              >
                 {title}
                 <br />
                 <span className="text-3xl">{subtitle}</span>
               </h1>
-              <button className="justify-center self-center px-16 py-5 mt-16 text-7xl text-amber-900 whitespace-nowrap bg-orange-200 rounded-3xl border-amber-900 border-solid border-[5px] max-md:pr-6 max-md:pl-6 max-md:mt-10 max-md:text-4xl">
+              <button className="mt-16 justify-center self-center whitespace-nowrap rounded-3xl border-[5px] border-solid border-amber-900 bg-orange-200 px-16 py-5 text-7xl text-amber-900 max-md:mt-10 max-md:pl-6 max-md:pr-6 max-md:text-4xl">
                 {buttonText}
               </button>
             </div>
           </div>
-          <div className="flex flex-col ml-5 w-6/12 max-md:ml-0 max-md:w-full">
+          <div className="ml-5 flex w-6/12 flex-col max-md:ml-0 max-md:w-full">
             <img
               loading="lazy"
               src={imageSrc}
               alt={imageAlt}
-              className="w-full aspect-square max-md:max-w-full"
+              className="aspect-square w-full max-md:max-w-full"
             />
           </div>
         </div>
