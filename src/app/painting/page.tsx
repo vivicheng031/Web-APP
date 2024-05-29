7022902236cdcbc27c567c8282f7d199eed7dc10
polinchen <90204339+berlin0308@users.noreply.github.com>
2024-05-29 16:25:35 +0800

    Painting tool (#24)
    * new paint page
    
    * painting completed
    
    * add painting function 2

--- src/app/painting/page.tsx
+++ src/app/painting/page.tsx
@@ -232,13 +232,14 @@ export default function Painting() {
 
   return (
     <div id="main-element" className="h-full">
-      <main className="h-min-full flex w-full flex-col bg-[#D1C3B9] xl:h-full">
+      <main className="h-min-full flex w-full flex-col bg-[#CFCFCF] xl:h-full">
         <div className="mx-24 my-4 flex flex-col items-center gap-2 text-yellow-600 xl:flex-row">
           <p className="text-6xl">{currentTopic}</p>
           <div className="grow"></div>
           <p className="text-4xl">deadline: {deadline}</p>
         </div>
 
+        {/* XL */}
         <div className="mx-24 hidden gap-5 xl:flex">
           <div className="relative aspect-[3/2] w-3/5 rounded-2xl border-4 border-[#E6B555] md:w-1/2">
             <div
@@ -255,12 +256,70 @@ export default function Painting() {
           </div>
           <div className="flex w-2/5 flex-col gap-4 md:w-1/2">
             <div className="flex flex-row gap-4">
-              <div className="relative aspect-[1/3] w-1/5 rounded-3xl bg-[#D9D9D9]"></div>
+              <div className="relative aspect-[1/3] w-1/4 rounded-3xl bg-[#D9D9D9] justify-center items-center">
+                <div className="my-16 flex flex-col gap-10 justify-center item-center h-full">
+                  <div className="flex flex-row gap-2 mx-2 justify-center item-center">
+                    <PiPaintBrushDuotone
+                      className={`h-full w-2/5 cursor-pointer self-center rounded-full p-1 ${
+                        brush && "bg-slate-100/50"
+                      }`}
+                      onClick={() => {
+                        setEraser(false);
+                        setBrush(true);
+                        setColor(displayColor);
+                      }}
+                    />
+
+                    <BsEraser
+                      className={`h-full w-2/5 cursor-pointer self-center rounded-full p-1 ${
+                        eraser && "bg-slate-100/50"
+                      }`}
+                      onClick={() => {
+                        setColor("#fff");
+                        setEraser(true);
+                        setBrush(false);
+                      }}
+                    />
+                  </div>
+
+                  <div className="flex w-full cursor-pointer self-center p-1 justify-center items-center">
+                    <input
+                      className="w-full mx-4"
+                      type="range"
+                      min="1"
+                      max="30"
+                      value={brushSize}
+                      onChange={handleSizeChange}
+                    />
+                  </div>
+
+                  <div className="flex w-full cursor-pointer self-center p-1 justify-center items-center">
+                    <ChromePicker
+                      className="z-3 mx-4 border-4 border-black rounded-2xl"
+                      color={displayColor}
+                      onChange={(e: ColorResult) => {
+                        setColor(e.hex);
+                        setDisplayColor(e.hex);
+                      }}
+                    />
+                  </div>
+
+                  <div className="h-1/6 flex w-full mw-220 justify-center items-center">
+                    <button
+                      type="button"
+                      className="w-full h-full mx-6 flex border-4 border-black rounded-4xl justify-center text-3xl flex items-center rounded-lg border-2 border-black px-2 py-2 text-black hover:bg-description/80"
+                      onClick={clear}
+                    >
+                      Clear
+                    </button>
+                  </div>
+                </div>
+              </div>
               <textarea
                 // onChange={(e) => setDescription(e.target.value)}
                 className="w-4/5 resize-none items-start rounded-2xl border-4 border-[#8B8B8B] bg-[#FBEFDF] px-4 py-2 text-4xl"
-                placeholder="Type description for your story..."
-                maxLength={100}
+                placeholder="Type something..."
+                maxLength={50}
               />
             </div>
             <div className="mx-4 grid grid-flow-col justify-stretch gap-6 text-5xl text-amber-700">
@@ -282,6 +341,7 @@ export default function Painting() {
           </div>
         </div>
 
+        {/* smaller than XL */}
         <div className="mx-24 flex flex-col gap-5 xl:hidden">
           <div className="relative aspect-[3/2] rounded-2xl border-4 border-[#E6B555]">
             <div
@@ -298,10 +358,10 @@ export default function Painting() {
           </div>
 
           <div className="flex flex-col gap-4">
-            <div className="flex flex-row gap-6">
-              <div className="flex flex-row gap-6">
-                <div className="justify-center item-center">
-                  <div className="flex flex-row gap-10 justify-center item-center">
+            <div className="flex flex-row w-7/8 gap-4 justify-center item-center mx-4">
+              <div className="flex flex-row w-1/2 justify-around">
+                <div className="gap-1 flex flex-col justify-center item-center">
+                  <div className="flex flex-row gap-2 justify-center item-center">
                     <PiPaintBrushDuotone
                       className={`h-[60px] w-[60px] cursor-pointer self-center rounded-full p-1 ${
                         brush && "bg-slate-100/50"
@@ -326,9 +386,7 @@ export default function Painting() {
                   </div>
                   <div className="flex flex-row gap-6">
                     <div
-                      className={
-                        "mt-2 h-full w-4/5 cursor-pointer self-center p-1"
-                      }
+                      className={"my-1 h-full cursor-pointer self-center p-3"}
                     >
                       <input
                         type="range"
@@ -339,24 +397,24 @@ export default function Painting() {
                       />
                     </div>
 
-                    <div
+                    {/* <div
                       className={`cursor-pointer self-center rounded-full bg-black item-center justify-center w-full`}
                       style={{
                         height: `${brushSize}px`,
                         width: `${brushSize}px`,
                         borderRadius: "50%",
                       }}
-                    ></div>
+                    ></div> */}
                   </div>
-                </div>
 
-                <button
-                  type="button"
-                  className="border-4 border-black rounded-2xl justify-center text-3xl w-[200px] flex items-center rounded-lg border-2 border-black px-2 my-14 text-black hover:bg-description/80"
-                  onClick={clear}
-                >
-                  Clear
-                </button>
+                  <button
+                    type="button"
+                    className="border-4 border-black rounded-2xl justify-center text-3xl flex items-center rounded-lg border-2 border-black px-2 mx-6 text-black hover:bg-description/80"
+                    onClick={clear}
+                  >
+                    Clear
+                  </button>
+                </div>
 
                 <div className="h-full cursor-pointer self-center p-1">
                   <ChromePicker
@@ -368,15 +426,14 @@ export default function Painting() {
                     }}
                   />
                 </div>
-
-                <div>
-                  <textarea
-                    // onChange={(e) => setDescription(e.target.value)}
-                    className="w-4/5 h-full resize-none items-start rounded-2xl border-4 border-[#8B8B8B] bg-[#FBEFDF] px-4 py-2 text-2xl"
-                    placeholder="Type description for your story..."
-                    maxLength={100}
-                  />
-                </div>
+              </div>
+              <div className="w-1/2">
+                <textarea
+                  // onChange={(e) => setDescription(e.target.value)}
+                  className="w-full h-full resize-none items-start rounded-2xl border-4 border-[#8B8B8B] bg-[#FBEFDF] px-4 py-2 text-3xl"
+                  placeholder="Type description for your story..."
+                  maxLength={100}
+                />
               </div>
             </div>
 
@@ -401,48 +458,6 @@ export default function Painting() {
       </main>
     </div>
   );
-}
-
-{
-  /* <AlertDialogContent>
-            <AlertDialogHeader>
-              <AlertDialogTitle className="text-2xl">
-                Are you sure you want to post?
-              </AlertDialogTitle>
-            </AlertDialogHeader>
-            <AlertDialogFooter>
-              <AlertDialogCancel onClick={() => setIsConfirmed(false)}>
-                Back
-              </AlertDialogCancel>
-              <AlertDialogAction onClick={handlePostClick}>
-                Confirm
-                {loading && (
-                  <div className="fixed left-0 top-0 flex h-full w-full items-center justify-center bg-gray-800 bg-opacity-75 text-2xl">
-                    <div role="status">
-                      <svg
-                        aria-hidden="true"
-                        className="h-8 w-8 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600"
-                        viewBox="0 0 100 101"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                          fill="currentColor"
-                        />
-                        <path
-                          d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                          fill="currentFill"
-                        />
-                      </svg>
-                      <span className="sr-only">Loading...</span>
-                    </div>
-                  </div>
-                )}
-              </AlertDialogAction>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialog> */
 }
 
 {
